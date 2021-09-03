5d74c407508e326b09151a4d2fc8a8de8839411d
Devishi Ranjan <drconker@gmail.com>
2021-09-04 01:11:45 +0530

    Added sidebar collapsing function

--- discord-theme.js
+++ discord-theme.js
@@ -2,4 +2,9 @@ $(document).ready( function() {
 	$("#darklight").click( function() {
 		$("body").toggleClass("dark light");
 	});
+
+	$("#servers .more").click( function() {
+		$(this).toggleClass("active");
+		$("#sidebar").toggleClass("active");
+	});
 });
