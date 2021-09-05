d8f1e4e5fd7b396181f909908f93849ffd600b22
Devishi Ranjan <drconker@gmail.com>
2021-09-06 05:19:11 +0530

    Added more tooltip sources

--- discord-theme.js
+++ discord-theme.js
@@ -28,5 +28,5 @@ $(document).ready( function() {
 		theme: "default"
 	});
 	
-	tippy(".timestamp [data-tippy-content]", { theme: "default" });
+	tippy(".timestamp [data-tippy-content], .actions [data-tippy-content]", { theme: "default" });
 });
